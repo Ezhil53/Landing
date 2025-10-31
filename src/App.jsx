64293a34a0716905c9ca10b91ea94a50b4e5e8c3
Ezhil-Dev <johnnydepp1853@gmail.com>
2025-10-31 18:57:16 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
-import React from 'react'
+
 import Home from './pages/Home'
 import Hero from './pages/Hero'
-import Noise from './components/Noise'
-import Header from './components/Header'
-import Films from './components/Films'
+
 
 const App = () => {
   return (
     <>
-      {/*  <div className='fixed top-0 w-full h-screen -z-10'>
-          <Noise />
-        </div>*/}
-        {/* <Header /> */}
-        {/* <Films/> */}
-       {/* <Home /> */}
         <Hero/>
     </>
   )
